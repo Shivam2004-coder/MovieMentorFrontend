40feeff430e6adaf5c087f0f6df0289815e46f17
VShivam0808 <vshivamgithub003@gmail.com>
2025-03-20 15:08:53 +0530

    Corrected the footer

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -14,7 +14,7 @@ const Library = () => {
   };
 
   return (
-    <div>
+    <div >
       <img 
           alt="library-images" 
           src="/libraryImage1.jpg" 
@@ -26,22 +26,24 @@ const Library = () => {
           <h1 className="text-4xl md:text-7xl font-bold mb-9 text-center">{translations.MyLibrary}</h1>
         </div>
         {Object.keys(playlists).length > 0 ? (
-          Object.keys(playlists).map((playlistName) => (
-            <div key={playlistName} className="bg-slate-700 bg-opacity-85 p-2 m-1 rounded-sm">
-              <MovieList title={playlistName} movies={playlists[playlistName]} />
-              {playlists[playlistName].length === 0 &&
-                <p className="text-black p-4 bg-gray-200 m-5 rounded-sm italic">{translations.Nomoviesaddedtothisplaylistyet}</p>
-              }
-              <button 
-                className="bg-black rounded-md p-4 ml-4 -mt-5 hover:scale-105 active:scale-100" 
-                onClick={() => handleDeletePlaylist(playlistName)} // Call function to delete
-              >
-                {translations.Delete}
-              </button>
-            </div>
-          ))
+          <div className="mb-48 md:mb-40" >
+            {Object.keys(playlists).map((playlistName) => (
+              <div key={playlistName} className="bg-slate-700 bg-opacity-85 p-2 m-1 rounded-sm">
+                <MovieList title={playlistName} movies={playlists[playlistName]} />
+                {playlists[playlistName].length === 0 &&
+                  <p className="text-black p-4 bg-gray-200 m-5 rounded-sm italic">{translations.Nomoviesaddedtothisplaylistyet}</p>
+                }
+                <button 
+                  className="bg-black rounded-md p-4 ml-4 -mt-5 hover:scale-105 active:scale-100" 
+                  onClick={() => handleDeletePlaylist(playlistName)} // Call function to delete
+                >
+                  {translations.Delete}
+                </button>
+              </div>
+            ))}
+          </div>
         ) : (
-          <div className="bg-slate-100 m-3" >
+          <div className="bg-slate-100 m-3 mb-[99%] md:mb-40" >
             <p className="text-black text-center">{translations.Noplaylistsavailable}</p>
           </div>
         )}
